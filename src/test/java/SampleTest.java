fa97314ea5802293e7f85090aa6ccd795e739a98
kjones2296 <33494570+kjones2296@users.noreply.github.com>
2019-04-04 23:12:09 -0700

    Add files via upload

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+	@Test
+	public void SimpleTest()
+	{
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+
+}
