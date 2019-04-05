0bbccadcd38e60c8cff38ce787aed6da9db3b80d
kjones2296 <33494570+kjones2296@users.noreply.github.com>
2019-04-04 23:02:10 -0700

    Add files via upload

---
+++ src/test/java/JenkinsPrezi/jenkins_demo/SampleTest.java
@@ -0,0 +1,13 @@
+package JenkinsPrezi.jenkins_demo;
+import org.junit.
+
+public class SampleTest {
+
+	@Test
+	public void simpleTest()
+	{
+		int a = 1;
+		int b = 2;
+		assertTrue(a + b == 3);
+	}
+}
